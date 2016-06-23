7a5cbca467dfe72573f2790be8891aba143f81be
Robb C <robbchar@gmail.com>
2016-06-23 19:16:42 -0400

    working two page app

---
+++ src/js/Nav.js
@@ -0,0 +1,17 @@
+export class Nav {
+  constructor (callback) {
+    this.nav = $("nav");
+
+    this.nav.on("click", "li", (event) => {
+      let element = $(event.target);
+      element
+        .addClass("active")
+        .siblings()
+          .removeClass("active");
+      callback(element.data("pageName"));
+    });
+
+    // call callback for active tab
+    callback(this.nav.find("li.active").data("pageName"));
+  }
+}
