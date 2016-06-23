b0fcb3d8243fe8bf56388aaf53d3de274e53b70f
Robb C <robbchar@gmail.com>
2016-06-23 19:16:20 -0400

    working two page app

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,2 +1,15 @@
+"use strict";
 require("../css/style.css");
-document.write(require("./content.js"));
+import * as main from "./Nav";
+
+const mainElement = $("main");
+const nav = new main.Nav((pageName) => {
+  switch (pageName) {
+    case "one":
+      mainElement.text(require("./pageOne.js"));
+    break;
+    case "two":
+      mainElement.text(require("./pageTwo.js"));
+    break;
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,96 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
-module.exports = {
+"use strict;"
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const merge = require('webpack-merge');
+const path = require('path');
+const validate = require('webpack-validator');
+const precss = require('precss');
+const autoprefixer = require('autoprefixer');
+
+// webpack config
+const devserverConfig = require('./webpack.config.devserver.js');
+const minifyConfig = require('./webpack.config.minify.js');
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+  dist: path.join(__dirname, 'dist')
+};
+
+const config = {
   entry: {
-    vendor: './src/js/vendor.js',
-    main: './src/js/entry.js'
+    vendor: PATHS.src + '/js/vendor.js',
+    entry: PATHS.src + '/js/entry.js'
   },
   output: {
-    path: __dirname + "/dist/",
+    path: PATHS.dist,
     filename: 'js/[name].[chunkhash].js'
   },
   module: {
-    loaders: [{
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-    }, {
-      test: /\.png$/,
-      //loader: 'file-loader'
-      loader: 'file-loader?name=/images/[name].[ext]' 
-    }, {
-      test: /\.html$/,
-      loader: 'html-loader'
-    }]
+    preLoaders: [
+      {
+        test: /\.(js|jsx)?$/,
+        loaders: ['eslint'],
+        include: PATHS.src
+      }
+    ],
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader')
+      }, {
+        test: /\.png$/,
+        loader: 'file-loader?name=/images/[name].[ext]' 
+      }, {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }, { 
+        test: /\.js$/,
+        include: path.join(__dirname, 'src'),
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  postcss: function () {
+      return [precss, autoprefixer];
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      favicon: 'src/favicon.ico',
-      template: 'src/index.html'
+      filename: 'main.html',
+      favicon: PATHS.src + '/favicon.ico',
+      template: PATHS.src + '/main.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'second.html',
+      favicon: PATHS.src + '/favicon.ico',
+      template: PATHS.src + '/second.html'
     }),
     new ExtractTextPlugin('css/styles.css')
   ]
 };
+
+switch(process.env.npm_lifecycle_event) {
+  case 'dev':
+    config = merge(
+      config,
+      {
+        devtool: 'source-map'
+      },
+      devserverConfig({
+        // Customize host/port here if needed
+        host: process.env.HOST,
+        port: process.env.PORT
+      })
+    );
+    break;
+  case 'build':
+    config = merge(
+      config,
+      minifyConfig()
+    );
+    break;
+}
+
+module.exports = validate(config);
